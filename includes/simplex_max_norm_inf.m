b262e069d62509dcfc0d557cd9faf33d14f18c2c
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-07-20 14:13:17 +0200

    Remove unused file

--- includes/simplex_max_norm_inf.m
+++
@@ -1,9 +0,0 @@
-function [norm_inf] = simplex_max_norm_inf(S)
-% -- [norm_inf] = simplex_max_norm_inf(S)
-%
-%     Compute max oriented length of a simplex S. Simplex points are
-%     represented as matrix columns.
-    N = size(S, 1);
-    V = S(:, 2:N+1) - S(:, ones(1,N));
-    norm_inf = max(max(abs(V)));
-end
