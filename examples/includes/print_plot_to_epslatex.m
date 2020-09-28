ca9fe7fb0f112d44f55a604e6738a9fe166581db
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-09-28 17:32:59 +0200

    Fix extra semicolon

--- examples/includes/print_plot_to_epslatex.m
+++ examples/includes/print_plot_to_epslatex.m
@@ -3,7 +3,7 @@ function print_plot_to_epslatex(plot_options)
 
     if (~is_octave() || field_empty(plot_options, 'print_path'))
         return;
-    end;
+    end
 
     print_size = strcat('-S', int2str(plot_options.print_size(1)), ',', int2str(plot_options.print_size(2)));
     print('-depslatex', '-mono', print_size, plot_options.print_path);
