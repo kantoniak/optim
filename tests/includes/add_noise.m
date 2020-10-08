c4964f30556b3eae3285a9fb8bed355adde18777
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-10-08 19:59:10 +0200

    Add noise generator to test suite

---
+++ tests/includes/add_noise.m
@@ -0,0 +1,16 @@
+
+function [value] = add_noise(f, V, rho, eta)
+% -- [value] = add_noise(f, V, rho, eta)
+%
+%     Add noise to value of function `f` using formula
+%
+%       f(V) + max(rho*abs(f(V)), eta) * mu
+%
+%     where `mu` is a random number generated with uniform distribution from
+%     range [-1, 1]. Use `rand('state', seed)` to set Mersenne Twister seed.
+
+    mu = unifrnd(-1, 1);
+    value = f(V);
+    value = value + max(rho*abs(value), eta) * mu;
+
+end
