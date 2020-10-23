93aaf8ee8c56140b152059ee36fb2d15dd4ef8a9
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-10-23 14:22:32 +0200

    Move test functions to a separate directory

--- init.m
+++ init.m
@@ -1,4 +1,5 @@
 addpath(strcat(pwd()));
 addpath(strcat(pwd(), filesep, 'includes'));
 addpath(strcat(pwd(), filesep, 'examples', filesep, 'includes'));
+addpath(strcat(pwd(), filesep, 'tests', filesep, 'functions'));
 addpath(strcat(pwd(), filesep, 'tests', filesep, 'includes'));
