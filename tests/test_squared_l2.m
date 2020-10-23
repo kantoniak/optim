1464304b56bb0f4c1161e0dd0d73ac16afbd9420
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-10-23 15:42:13 +0200

    Add test handling for custom implementations

--- tests/test_squared_l2.m
+++ tests/test_squared_l2.m
@@ -1,10 +1,10 @@
 % Function details
 n = 16;
 func = @squared_l2_norm_func;
-x0 = squared_l2_norm_point(n);
-minimizer_func = @fminsearch;
 func_name = 'squared_l2_norm';
-minimizer_func_name = 'fminsearch';
+x0 = squared_l2_norm_point(n);
+minimizer_func = @fminsearch_nm;
+minimizer_func_name = 'fminsearch_nm_restarts';
 output_dir = 'out/data/';
 
 run_test(n, func, func_name, x0, minimizer_func, minimizer_func_name, output_dir);
