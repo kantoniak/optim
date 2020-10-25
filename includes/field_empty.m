3c13b2f185d09f1dd31d4c3959e5580e51213943
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-10-25 19:13:27 +0100

    Move plot functions to global includes

--- includes/field_empty.m
+++ includes/field_empty.m
@@ -3,7 +3,7 @@ function [result] = field_empty(options, name)
 %
 %     Checks if structure field is empty.
 
-    if ~isfield(options, name)  || isempty(options.(name))
+    if ~isfield(options, name) || isempty(options.(name))
         result = true;
     else
         result = false;
--- includes/plot_history_fields.m
+++ includes/plot_history_fields.m
@@ -3,9 +3,9 @@ function plot_history_fields(entries, field_names, plot_dims, plot_options, prin
     field_count = size(field_names, 2);
     for idx = 1:field_count
         subplot(plot_dims(1), plot_dims(2), idx);
-        set_up_plot(plot_options);
+        set_up_plot(plot_options(idx));
         hold on
-            plot_history_field(entries, char(field_names(idx)));
+            plot_history_field(entries, char(field_names(idx)), []); % Empty field config
         hold off
     end
 
