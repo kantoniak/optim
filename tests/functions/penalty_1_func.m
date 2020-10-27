307ac0b818be1debc95ae0761bc51363f388982a
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-10-27 22:52:09 +0100

    Fix typos in function definitions

--- tests/functions/penalty_1_func.m
+++ tests/functions/penalty_1_func.m
@@ -3,7 +3,7 @@ function [value] = penalty_1_func(V)
     % Force vector
     V = V(:);
     n = size(V, 1);
-    U = 10^-5 * (V .- 1);
+    U = 10^-5 * (V .- 1).^2;
     U(n+1) = sum(V .^ 2) - 0.25;
     U(n+1) = U(n+1)^2;
     value = sum(U);
