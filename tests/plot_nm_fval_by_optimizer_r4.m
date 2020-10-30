5b5da950da9554b6e1016a687d3ff30066886fe8
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-10-30 15:06:59 +0100

    Simplify defining test_cases

--- tests/plot_nm_fval_by_optimizer_r4.m
+++ tests/plot_nm_fval_by_optimizer_r4.m
@@ -1,87 +1,42 @@
 % Plot best function value over iteration separately for each function.
-dimensions = [16];
+dimensions = [4];
 output_dir = 'out/data/';
 
 optimizers = struct('func', {});
-optimizers(1).func = @fminsearch;
-optimizers(1).func_name = 'fminsearch';
-optimizers(1).display_name = 'fminsearch';
-optimizers(1).line_style = ':';
+optimizers(1).func = @fminsearch_nm;
+optimizers(1).func_name = 'fminsearch_nm';
+optimizers(1).display_name = 'fminsearch\_nm';
+optimizers(1).line_style = '-';
 optimizers(1).line_width = 0.5;
-optimizers(2).func = @fminsearch_nm;
-optimizers(2).func_name = 'fminsearch_nm';
-optimizers(2).display_name = 'fminsearch\_nm';
-optimizers(2).line_style = '-';
+optimizers(2).func = @fminsearch;
+optimizers(2).func_name = 'fminsearch';
+optimizers(2).display_name = 'fminsearch';
+optimizers(2).line_style = ':';
 optimizers(2).line_width = 0.5;
 
 test_cases = struct('func', {});
 plot_options = struct('title', {});
 
-% Squared l2 norm
+% Objective functions
 test_cases(1).objective = get_objective_func('squared_l2_norm');
-test_cases(1).dimensions = dimensions;
-test_cases(1).optimizers = optimizers;
-test_cases(1).output_dir = output_dir;
-
-max_entry_count = get_max_entry_count(test_cases(1));
-plot_options(1).title = 'Squared l2 norm';
-plot_options(1).x_range = [0 max_entry_count];
-plot_options(1).show_legend = false;
-
-% Penalty I norm
 test_cases(2).objective = get_objective_func('penalty_1');
-test_cases(2).dimensions = dimensions;
-test_cases(2).optimizers = optimizers;
-test_cases(2).output_dir = output_dir;
-
-max_entry_count = get_max_entry_count(test_cases(2));
-plot_options(2).title = 'Penalty I function';
-plot_options(2).x_range = [0 max_entry_count];
-plot_options(2).show_legend = false;
-
-% Powell function
 test_cases(3).objective = get_objective_func('powell');
-test_cases(3).dimensions = dimensions;
-test_cases(3).optimizers = optimizers;
-test_cases(3).output_dir = output_dir;
-
-max_entry_count = get_max_entry_count(test_cases(3));
-plot_options(3).title = 'Powell function';
-plot_options(3).x_range = [0 max_entry_count];
-plot_options(3).show_legend = false;
-
-% Rosenbrock function
 test_cases(4).objective = get_objective_func('rosenbrock');
-test_cases(4).dimensions = dimensions;
-test_cases(4).optimizers = optimizers;
-test_cases(4).output_dir = output_dir;
-
-max_entry_count = get_max_entry_count(test_cases(4));
-plot_options(4).title = 'Rosenbrock function';
-plot_options(4).x_range = [0 max_entry_count];
-plot_options(4).show_legend = false;
-
-% Trigonometric function
 test_cases(5).objective = get_objective_func('trigonometric');
-test_cases(5).dimensions = dimensions;
-test_cases(5).optimizers = optimizers;
-test_cases(5).output_dir = output_dir;
-
-max_entry_count = get_max_entry_count(test_cases(5));
-plot_options(5).title = 'Trigonometric function';
-plot_options(5).x_range = [0 max_entry_count];
-plot_options(5).show_legend = false;
-
-% Variably dimensioned function
 test_cases(6).objective = get_objective_func('variably_dimensioned');
-test_cases(6).dimensions = dimensions;
-test_cases(6).optimizers = optimizers;
-test_cases(6).output_dir = output_dir;
 
-max_entry_count = get_max_entry_count(test_cases(6));
-plot_options(6).title = 'Variably dimensioned function';
-plot_options(6).x_range = [0 max_entry_count];
-plot_options(6).show_legend = false;
+% Set common settings
+case_count = size(test_cases, 2);
+for i=1:case_count
+    test_cases(i).dimensions = dimensions;
+    test_cases(i).optimizers = optimizers;
+    test_cases(i).output_dir = output_dir;
+
+    max_entry_count = get_max_entry_count(test_cases(i));
+    plot_options(i).title = test_cases(i).objective.display_name;
+    plot_options(i).x_range = [0 max_entry_count];
+    plot_options(i).show_legend = false;
+end
 
 % Plot
 plot_dims = [3 2];
