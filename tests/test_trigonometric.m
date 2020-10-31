1f68aad5407c23815995cdb1e17a6666715fe9da
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-11-01 00:40:38 +0100

    Add more dimensions to testing procedures

--- tests/test_trigonometric.m
+++ tests/test_trigonometric.m
@@ -1,6 +1,6 @@
 % Function details
 test_case.objective = get_objective_func('trigonometric');
-test_case.dimensions = [4 16];
+test_case.dimensions = [1 4 8 12 16 20 24 28 32];
 test_case.output_dir = 'out/data/';
 test_case.optimizers = get_default_optimizers();
 
