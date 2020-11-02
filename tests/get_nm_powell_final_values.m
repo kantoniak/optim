fddc2fbca2f4414517c9d2c7b2262d82ab72a6dd
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-11-02 16:38:59 +0100

    Update Powell final values with extra data

--- tests/get_nm_powell_final_values.m
+++ tests/get_nm_powell_final_values.m
@@ -14,9 +14,9 @@ test_case.optimizers(1).line_width = 0.5;
 file = fopen(output_filename, 'w');
 fprintf(file, '\\begin{table}[]\n');
 fprintf(file, '\\centering\n');
-fprintf(file, '\\begin{tabular}{|c|c|c|c|}\n');
+fprintf(file, '\\begin{tabular}{|c|c|c|c|c|}\n');
 fprintf(file, '\\hline\n');
-fprintf(file, '$n$ & Iterations ($k$) & $ \\norm{x^k_1} $ & $ f(x^k_1) $ \\\\ \\hline\n');
+fprintf(file, '$n$ & Iterations ($k$) & Evaluations of $f$ & $ \\norm{x^k_1} $ & $ f(x^k_1) $ \\\\ \\hline\n');
 
 % Run for all dimensions
 dimension_count = size(test_case.dimensions, 2);
@@ -29,11 +29,12 @@ for d=1:dimension_count
 
     final_entry = entries(end);
     iters = final_entry.iter;
+    fcount = final_entry.fcount;
     x_norm = norm(final_entry.x, 2);
     fval = final_entry.fval;
 
     % Print line
-    fprintf(file, '%d & \\multicolumn{1}{r|}{%d} & \\multicolumn{1}{r|}{%f} & \\multicolumn{1}{r|}{%f}', dim, iters, x_norm, fval);
+    fprintf(file, '%d & \\multicolumn{1}{r|}{%d} & \\multicolumn{1}{r|}{%d} & \\multicolumn{1}{r|}{%.12f} & \\multicolumn{1}{r|}{%.12f}', dim, iters, fcount, x_norm, fval);
     fprintf(file, ' \\\\ \\hline\n');
 
 end
