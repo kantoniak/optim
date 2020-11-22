45b0ac94a7a9457f2b323f0ef39c5e25a06fce18
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-11-22 19:00:02 +0100

    Matlab cooperability: change file names

--- tests/includes/get_output_filename.m
+++ tests/includes/get_output_filename.m
@@ -3,5 +3,5 @@ function [output_filename] = get_output_filename(output_dir, optimizer, func_nam
 %
 %     Generates filename for test data.
 
-    output_filename = sprintf('%s%s-%s-%d.mat', output_dir, optimizer.func_name, func_name, n);
+    output_filename = sprintf('%s%s_%s_%d.mat', output_dir, optimizer.func_name, func_name, n);
 end
